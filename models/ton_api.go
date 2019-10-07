e380f4f9cff6c7f20f9ddb0c0356f782fa22584a
shpana <a.shaidullin@rdl-telecom.com>
2019-10-07 20:45:27 +0300

    Create Template for structs

---
+++ models/ton_api.go
@@ -0,0 +1,334 @@
+package models
+
+
+
+// This class is a base class for all TDLib interface classes.
+type TonAPI struct {
+
+}
+
+type AccountAddress struct {
+
+}
+
+type Bip39Hints struct {
+
+}
+
+type Config struct {
+
+}
+
+type Error struct {
+
+}
+
+type ExportedEncryptedKey struct {
+
+}
+
+type ExportedKey struct {
+
+}
+
+type ExportedPemKey struct {
+
+}
+
+type InputKey struct {
+
+}
+
+type Key struct {
+
+}
+
+type KeyStoreType struct {
+
+}
+
+type KeyStoreTypeDirectory struct {
+
+}
+
+type KeyStoreTypeInMemory struct {
+
+}
+
+type LogStream struct {
+
+}
+
+type LogStreamDefault struct {
+
+}
+
+type LogStreamFile struct {
+
+}
+
+type LogStreamEmpty struct {
+
+}
+
+type LogTags struct {
+
+}
+
+type LogVerbosityLevel struct {
+
+}
+
+type Ok struct {
+
+}
+
+type Options struct {
+
+}
+
+
+type SendGramsResult struct {
+
+}
+
+type UnpackedAccountAddress struct {
+
+}
+
+type UpdateSendLiteServerQuery struct {
+
+}
+
+type GenericAccountState struct {
+
+}
+
+type GenericAccountStateRAW struct {
+
+}
+
+type GenericAccountStateTestWallet struct {
+
+}
+
+type GenericAccountStateWallet struct {
+
+}
+
+type GenericAccountStateTestGiver struct {
+
+}
+
+type GenericAccountStateUninited struct {
+
+}
+
+type InternalTransactionID struct {
+
+}
+
+type RAWAccountState struct {
+
+}
+
+type RAWInitialAccountState struct {
+
+}
+
+type RAWMessage struct {
+
+}
+
+type RAWTransaction struct {
+
+}
+
+type TestGiverAccountState struct {
+
+}
+
+type TestWalletAccount struct {
+
+}
+
+type TestWalletInitialAccountState struct {
+
+}
+
+type UninitedAccountState struct {
+
+}
+
+type WalletAccountState struct {
+
+}
+
+type WalletInitialAccountState struct {
+
+}
+
+type AddLogMessage struct {
+
+}
+
+type ChangeLocalPassword struct {
+
+}
+
+type Close struct {
+
+}
+
+type CreateNewKey struct {
+
+}
+
+type DeleteKey struct {
+
+}
+
+type ExportEncryptedKey struct {
+
+}
+
+type ExportKey struct {
+
+}
+
+type ExportPemKey struct {
+
+}
+
+type GenericGetAccountState struct {
+
+}
+
+type GenericSendGrams struct {
+
+}
+
+type GetBip39Hints struct {
+
+}
+
+type GetLogStream struct {
+
+}
+
+type GetLogTagVerbosityLevel struct {
+
+}
+
+type GetLogsTags struct {
+
+}
+
+type GetLogVerbosityLevel struct {
+
+}
+
+type ImportEncryptedKey struct {
+
+}
+
+type ImportKey struct {
+
+}
+
+type Init struct {
+
+}
+
+type OnLiteServerQueryError struct {
+
+}
+
+type OnLiteServerQueryResult struct {
+
+}
+
+type OptionsSetConfig struct {
+
+}
+
+type PackAccountAddress struct {
+
+}
+
+type RAWGetAccountAddress struct {
+
+}
+
+type RAWGetAccountState struct {
+
+}
+
+type RAWGetTransactions struct {
+
+}
+
+type RAWSendMessage struct {
+
+}
+
+type RunTests struct {
+
+}
+
+type SetLogStream struct {
+
+}
+
+type SetLogTagVerbosityLevel struct {
+
+}
+
+type SetLogVerbosityLevel struct {
+
+}
+
+type TestGiverGetAccountAddress struct {
+
+}
+
+type TestGiverGetAccountState struct {
+
+}
+
+type TestGiverSendGrams struct {
+
+}
+
+type TestWalletGetAccountAddress struct {
+
+}
+
+type TestWalletGetAccountState struct {
+
+}
+
+type TestWalletInit struct {
+
+}
+
+type TestWalletSendGrams struct {
+
+}
+
+type UnpackAccountAddress struct {
+
+}
+
+type WalletGetAccountAddress struct {
+
+}
+
+type WalletGetAccountState struct {
+
+}
+
+
+
+
+
